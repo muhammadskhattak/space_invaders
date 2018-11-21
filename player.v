11d8872611c84e72d6f228f64c5c42f0f6a77476
Muhammad Khattak <muhammadskhattak@gmail.com>
2018-11-21 14:49:35 +0000

    added method header for player

---
+++ player.v
@@ -0,0 +1,9 @@
+module player(clk, reset_n, p_up, p_down, y_posin, x_pos,y_pos);
+    input clk;
+    input reset_n;
+    input p_up;
+    input p_down;
+    input [6:0] y_posin;
+    output [7:0] x_pos;
+    output [7:0] y_pos;
+endmodule
