b1044bc9d401a410dba3cf880dfa30e10f821d97
Muhammad Khattak <muhammadskhattak@gmail.com>
2018-11-22 03:24:02 +0000

    Implemented datapath and controller in fpga top module

--- player.v
+++ player.v
@@ -6,12 +6,12 @@ module player(clk, reset_n, add_x, add_y, y_pos_mod, y_neg_mod, x_pos, y_pos);
     input y_pos_mod; // Indicates when to move the entire "ship" up
     input y_neg_mod;  // Indicates when to move the entire "ship" down
     output [7:0] x_pos; // Output to VGA
-    output [7:0] y_pos; // Output to VGA
-	 
+    output [6:0] y_pos; // Output to VGA
+
 	 reg [7:0] x_pixel = 8'b10011011;
-	 reg [7:0] y_pixel;
+	 reg [6:0] y_pixel;
 	 reg y_increment;
-	 
+
 	 //Determines what y increment is
     always @(posedge clk) begin
 		if (!reset_n)
@@ -29,5 +29,5 @@ module player(clk, reset_n, add_x, add_y, y_pos_mod, y_neg_mod, x_pos, y_pos);
     end
 	 assign x_pos = x_pixel + add_x;
 	 assign y_pos = add_y + y_pixel;
-	 
+
 endmodule
